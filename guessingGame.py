497c01da3c8ae318cf7c0067ef6f670c38086027
ShreyashiMahanta <53649054+ShreyashiMahanta@users.noreply.github.com>
2020-07-10 20:43:40 +0530

    Add files via upload

---
+++ guessingGame.py
@@ -0,0 +1,7 @@
+number=int(input("Guess any number from 1-9"))
+if(number>6):
+    print("Nope,thats not it!!")
+elif(number<6):
+    print("Uh uh,thats not it!!")
+else:
+    print("YAY!!thats right")
